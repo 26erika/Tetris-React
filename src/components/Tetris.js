cd9785d16c33a2c4ca1eb6fbe4b4df8deb2cd972
26erika <develop.erika@gmail.com>
2020-06-13 13:44:12 -0500

    Player rotation

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -19,7 +19,7 @@ const Tetris = () => {
 const [dropTime, setDropTime] = useState(null);
 const [gameOver, setGameOver] = useState(false);
 
-const [player, updatePlayerPos, resetPlayer] = usePlayer();
+const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
 const [stage, setStage] = useStage(player, resetPlayer);
 
     console.log('re-render');
@@ -59,6 +59,8 @@ const [stage, setStage] = useStage(player, resetPlayer);
                 movePlayer(1);
             }else if (keyCode === 40) {
                 dropPlayer();
+            }else if (keyCode === 38) {
+                playerRotate(stage, 1);
             }
         }
     }

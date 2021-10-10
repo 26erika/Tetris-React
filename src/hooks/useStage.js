48bd47e977adc6ed212360f529ab4e59fb7a93a3
26erika <develop.erika@gmail.com>
2021-10-10 18:52:17 -0500

    Set rows cleared

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -5,10 +5,24 @@ export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
 
     useEffect(()=> {
+        setRowsCleared(0);
+        const sweepRows = newStage =>
+            newStage.reduce((ack, row) => {
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared(prev => prev + 1);
+                    ack.unShift(new Array(newStage[0].length).fill([0,'clear']));
+                    return ack;
+                }
+                ack.push(row);
+                return ack;
+            }, []);
+
         const updateStage = prevStage => {
             // First flush the stage
-            const newStage = prevStage.map(row => row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+            const newStage = prevStage.map(row => 
+                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
             );
+
             // Then draw the tetromino
             player.tetromino.forEach((row, y) => {
                 row.forEach((value, x) => {
@@ -22,6 +36,7 @@ export const useStage = (player, resetPlayer) => {
             // then check if we collided
             if (player.collided) {
                 resetPlayer(0);
+                return sweepRows(newStage);
             }
 
             return newStage;
